5324ec24260242296c96661f00cef2651fbe9704
blindlp <luispoz@gmail.com>
2020-07-12 20:09:53 -0600

    Message recibe and convert in standar

--- omnichannel-server/src/index.ts
+++ omnichannel-server/src/index.ts
@@ -1,21 +1,18 @@
 import { Dependencies } from "@corecodeio/libraries/di";
 import { Request, Response } from "express";
-import { MessagePayload } from "./feature/message-source/interface/MessagePayload";
+import { MessageDigestInjectionKey } from "./feature/message-source/MessageDigestInjectionKey";
 import { MessageSourceControllerInjectionKey } from "./feature/message-source/MessageSourceControllerInjectionKey";
 import server from "./server";
 
 const dependencies = new Dependencies();
 
 server.post("/message-source", async (req: Request, res: Response) => {
+  const data = req.body;
+  const messageDigest = dependencies.provide(MessageDigestInjectionKey);
+
   const MessageSourceController = dependencies.provide(
     MessageSourceControllerInjectionKey
   );
-
-  const payload: MessagePayload = {
-    message: "",
-    currentTime: new Date(),
-    from: "",
-  };
 
   // recibir la solicitud (payload de twilio o messagebird MessageSourceProvider)
   // estandarizar la solicitud (MessageSource{Digest|Parser} || MessageFormatter)
