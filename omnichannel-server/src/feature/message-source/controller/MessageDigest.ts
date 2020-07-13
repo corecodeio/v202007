1109b5add1e54cf9c74802e71003ee3e635c6bf0
blindlp <luispoz@gmail.com>
2020-07-12 20:08:51 -0600

    Message Digest parser message

--- omnichannel-server/src/feature/message-source/controller/MessageDigest.ts
+++ omnichannel-server/src/feature/message-source/controller/MessageDigest.ts
@@ -1,1 +1,28 @@
-export class MessageDigest {}
+import { MessageBirdPayload } from "../interface/MessageBirdPayload";
+import { MessagePayload } from "../interface/MessagePayload";
+
+export class MessageDigest {
+  private data: any;
+  private messageBirdStandar: MessageBirdPayload = {
+    contactPhoneNumber: "",
+    currentTime: new Date(),
+    payload: "",
+  };
+  private message: MessagePayload = {
+    message: "",
+    currentTime: new Date(),
+    from: "",
+  };
+
+  constructor() {}
+
+  public set setData(data) {
+    this.message.message = this.messageBirdStandar.contactPhoneNumber;
+    this.message.currentTime = this.messageBirdStandar.currentTime;
+    this.message.from = this.messageBirdStandar.payload;
+  }
+
+  public get getData() {
+    return this.message;
+  }
+}
