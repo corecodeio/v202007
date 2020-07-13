45e6e6235b382a1a3fe52b0515988b2d9896602c
danilo <danilo_solorzano06@hotmail.com>
2020-07-13 16:18:10 -0600

    update prettier

--- omnichannel-server/src/feature/message-source/MessageParser.ts
+++ omnichannel-server/src/feature/message-source/MessageParser.ts
@@ -4,65 +4,63 @@ import { MessagePayload } from "./interfaces/MessagePayload";
 import { MessageProvider } from "./interfaces/MessageProvider";
 
 export class MessageParser {
+  messageSource(req: Request, res: Response): Promise<Response> {
+    const payload = this.parseRequestBody(req.body);
 
-messageSource(req: Request, res: Response):Promise<Response>{
-  const payload = this.parseRequestBody(req.body)
+    // TODO: falta codigo
 
-  // TODO: falta codigo
-
-  return res.json({ status: "success" })
-}
-
-parseRequestBody = (body) => {
-  const messageBody = <MessageBirdPayload | MessageTwilioPayload>body;
-
-  if (this.isMessageBirdPayload(<MessageBirdPayload>messageBody)) {
-    const MessageBody: MessagePayload = {
-      from: (<MessageBirdPayload>messageBody).contactPhoneNumber,
-      message: (<MessageBirdPayload>messageBody).payload,
-      currentTime: (<MessageBirdPayload>messageBody).currentTime,
-      messageProvider: MessageProvider.messageBird,
-    };
-
-    return MessageBody;
-  } else if (this.isTwilioPayload(<MessageTwilioPayload>messageBody)) {
-    const MessageBody: MessagePayload = {
-      from: (<MessageTwilioPayload>messageBody).Number,
-      message: (<MessageTwilioPayload>messageBody).Message,
-      currentTime: (<MessageTwilioPayload>messageBody).Time,
-      messageProvider: MessageProvider.twilio,
-    };
-
-    return MessageBody;
+    return res.json({ status: "success" });
   }
-};
 
-responseMessage = (message: MessagePayload) => {
-  if (message.messageProvider === MessageProvider.messageBird) {
-    const MessageBody: MessageBirdPayload = {
-      contactPhoneNumber: message.from,
-      payload: message.message,
-      currentTime: message.currentTime,
-    };
+  parseRequestBody = (body) => {
+    const messageBody = <MessageBirdPayload | MessageTwilioPayload>body;
 
-    return MessageBody;
-  } else if (message.messageProvider === MessageProvider.twilio) {
-    const MessageBody: MessageTwilioPayload = {
-      Number: message.from,
-      Message: message.message,
-      Time: message.currentTime,
-    };
+    if (this.isMessageBirdPayload(<MessageBirdPayload>messageBody)) {
+      const MessageBody: MessagePayload = {
+        from: (<MessageBirdPayload>messageBody).contactPhoneNumber,
+        message: (<MessageBirdPayload>messageBody).payload,
+        currentTime: (<MessageBirdPayload>messageBody).currentTime,
+        messageProvider: MessageProvider.messageBird,
+      };
 
-    return MessageBody;
-  }
-};
+      return MessageBody;
+    } else if (this.isTwilioPayload(<MessageTwilioPayload>messageBody)) {
+      const MessageBody: MessagePayload = {
+        from: (<MessageTwilioPayload>messageBody).Number,
+        message: (<MessageTwilioPayload>messageBody).Message,
+        currentTime: (<MessageTwilioPayload>messageBody).Time,
+        messageProvider: MessageProvider.twilio,
+      };
 
-private isMessageBirdPayload = (message: MessageBirdPayload): boolean => {
-  return message.contactPhoneNumber !== undefined;
-};
+      return MessageBody;
+    }
+  };
 
-private isTwilioPayload = (message: MessageTwilioPayload): boolean => {
-  return message.Number !== undefined;
-};
+  responseMessage = (message: MessagePayload) => {
+    if (message.messageProvider === MessageProvider.messageBird) {
+      const MessageBody: MessageBirdPayload = {
+        contactPhoneNumber: message.from,
+        payload: message.message,
+        currentTime: message.currentTime,
+      };
 
+      return MessageBody;
+    } else if (message.messageProvider === MessageProvider.twilio) {
+      const MessageBody: MessageTwilioPayload = {
+        Number: message.from,
+        Message: message.message,
+        Time: message.currentTime,
+      };
+
+      return MessageBody;
+    }
+  };
+
+  private isMessageBirdPayload = (message: MessageBirdPayload): boolean => {
+    return message.contactPhoneNumber !== undefined;
+  };
+
+  private isTwilioPayload = (message: MessageTwilioPayload): boolean => {
+    return message.Number !== undefined;
+  };
 }
