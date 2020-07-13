43dbe2d008ea4a617838346d0b2d474d376a7ee1
danilo <danilo_solorzano06@hotmail.com>
2020-07-13 09:59:15 -0600

    resolve duplicación de archivos

--- libraries/interfaces/InjectionKey.ts
+++
@@ -1,9 +0,0 @@
-import { IDependiecies } from "../di/iDependecies";
-import { InjectionKeyScope } from "./InjectionKeyScope";
-
-
-export interface InjectionKey<T> {
-    name: string;
-    scope: InjectionKeyScope;
-    clousure: (dependencies: IDependiecies) => T;
-}
