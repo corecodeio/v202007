1c8831ea88a536fb56016c1efe87bd2f43aa7367
blindlp <luispoz@gmail.com>
2020-07-13 21:46:45 -0600

    Binds messageSourceController to method

--- omnichannel-server/src/index.ts
+++ omnichannel-server/src/index.ts
@@ -1,28 +1,24 @@
 import { Dependencies } from "@corecodeio/libraries/di";
-import { Request, Response } from "express";
-import { MessageDigestInjectionKey } from "./feature/message-source/MessageDigestInjectionKey";
-import { MessageSourceControllerInjectionKey } from "./feature/message-source/MessageSourceControllerInjectionKey";
+import { MessageSourceController } from "./feature/message-source/controller/MessageSourceController";
+import { MessageSourceControllerInjectionKey } from "./feature/message-source/InjectionKeys";
 import server from "./server";
 
 const dependencies = new Dependencies();
+const messageSourceController = dependencies.provide<MessageSourceController>(
+  MessageSourceControllerInjectionKey
+);
 
-server.post("/message-source", async (req: Request, res: Response) => {
-  const data = req.body;
-  const messageDigest = dependencies.provide(MessageDigestInjectionKey);
+server.post(
+  "/message-source",
+  messageSourceController.messageSource.bind(messageSourceController)
+);
 
-  const MessageSourceController = dependencies.provide(
-    MessageSourceControllerInjectionKey
-  );
-
-  // recibir la solicitud (payload de twilio o messagebird MessageSourceProvider)
-  // estandarizar la solicitud (MessageSource{Digest|Parser} || MessageFormatter)
-  // guardar la metadata del mensaje en una base de datos
-  // notificar a maría (native client) que recibió un mensaje
-  // PostRequestServer, MVC, MessageFormatter, MessageController
-  // Dependency Injection
-
-  return res.status(200);
-});
+// recibir la solicitud (payload de twilio o messagebird MessageSourceProvider)
+// estandarizar la solicitud (MessageSource{Digest|Parser} || MessageFormatter)
+// guardar la metadata del mensaje en una base de datos
+// notificar a maría (native client) que recibió un mensaje
+// PostRequestServer, MVC, MessageFormatter, MessageController
+// Dependency Injection
 
 server.listen("8001", () => {
   console.log("listening");
