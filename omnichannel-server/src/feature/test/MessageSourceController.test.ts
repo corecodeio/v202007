47dbb581c02d43411fca9853491a84322eab2462
Jimmy <jimmyottonielgr@gmail.com>
2020-07-15 03:20:39 -0600

    Completing the test for MessageSourceController.test.ts

--- omnichannel-server/src/feature/test/MessageSourceController.test.ts
+++ omnichannel-server/src/feature/test/MessageSourceController.test.ts
@@ -1,11 +1,26 @@
 import { Dependencies } from "@corecodeio/libraries/di";
 import { MessageSourceControllerInjectionKey } from "../message-source/InjectionKeys";
+import { Request, Response } from "express";
 
 const dependencies = new Dependencies();
-const messageSourceDigest = dependencies.provide(
+const messageSourceController = dependencies.provide(
   MessageSourceControllerInjectionKey
 );
 
 describe("Testing MessageSourceController messageSource method", () => {
-  test("Should return something", () => {});
+  test("The result of the action is transmitted in the body", () => {
+    const req: Request = {
+      body: {
+        contactPhoneNumber: +7456123,
+        payload: "Jest",
+        currentTime: "new Date()",
+      },
+    };
+
+    const res: Response = {};
+
+    const status = messageSourceController.messageSource(res, res);
+
+    //expect(req.status(200).toEqual(status.req.status(200)));
+  });
 });
