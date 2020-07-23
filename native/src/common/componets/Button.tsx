ff820525d4bfa7bcce2bcdbbf2275eae853c124f
blindlp <luispoz@gmail.com>
2020-07-22 20:03:36 -0600

    add native expo-app

---
+++ native/src/common/componets/Button.tsx
@@ -0,0 +1,13 @@
+import { TouchableOpacity } from "react-native";
+
+import React from 'react';
+
+
+export const MainActionButton = () => {
+    return (
+        <TouchableOpacity
+            style={{ backgroundColor: "black", padding: 21, margin: 23 }}>
+            <text style={{ color: "#fff " }}></text>
+        </TouchableOpacity>
+    );
+};
