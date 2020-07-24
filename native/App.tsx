47fca00d1817396e315a06431750a04a7dcc3629
pugaIsaias <64232550+pugaIsaias@users.noreply.github.com>
2020-07-24 11:36:27 -0600

    Used functions instead of class components

--- native/App.tsx
+++ native/App.tsx
@@ -1,22 +1,76 @@
-import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
-class OnboardingWelcome extends React.Component {
-	render() {
-		return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createStackNavigator();
+
+function OnboardingCompleted() {
+	return (
+		<View>
+			<Text>Great! Now try to connect. Enjoy!</Text>
+		</View>
+	);
+}
+
+function OnboardingStoreConnection() {
+	return (
+		<View>
+			<Text>Now connect to your store</Text>
+			<TextInput></TextInput>
+		</View>
+	);
+}
+
+function OnboardingPhoneVerification() {
+	return (
+		<View>
+			<Text>Insert verification code</Text>
+			<TextInput></TextInput>
+		</View>
+	);
+}
+
+function OnboardingWelcome() {
+	return (
+		<View>
+			<Text>Welcome! Input your whatsapp number</Text>
+			<TextInput></TextInput>
+			{/*<Button
+				title="Get Started"
+				onPress={() => navigation.navigate("OnboardingPhoneVerification")}
+      />*/}
+		</View>
+	);
+}
+
+export default function App() {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen name="OnboardingWelcome" component={OnboardingWelcome} />
+				<Stack.Screen
+					name="OnboardingPhoneVerification"
+					component={OnboardingPhoneVerification}
+				/>
+				<Stack.Screen
+					name="OnboardingStoreConnection"
+					component={OnboardingStoreConnection}
+				/>
+				<Stack.Screen
+					name="OnboardingCompleted"
+					component={OnboardingCompleted}
+				/>
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
