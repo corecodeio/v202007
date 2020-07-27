ab71110504a35fb7c116c3b2be4755bf82a2812f
blindlp <luispoz@gmail.com>
2020-07-27 17:30:20 -0600

    Create MainActionButton

---
+++ native/src/common/component/MainActionButton.tsx
@@ -0,0 +1,12 @@
+import { TouchableOpacity, Text } from "react-native";
+import React from "react";
+
+export const MainActionButton = () => {
+  return (
+    <TouchableOpacity
+      style={{ backgroundColor: "black", padding: 21, margin: 23 }}
+    >
+      <Text style={{ color: "#fff" }}></Text>
+    </TouchableOpacity>
+  );
+};
