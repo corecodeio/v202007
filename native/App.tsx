04e05e4f92db2903cea4321da75897e620f02cad
Ktoxcon <kennethtoxro@gmail.com>
2020-08-02 12:33:22 -0600

    Update: ApolloProvider added

--- native/App.tsx
+++ native/App.tsx
@@ -1,19 +1,26 @@
-import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
+import OnboardingNavigator from "./src/navigation/navigator/OnboardingNavigator";
+import { Client } from "./src/feature/graphql/client";
+import { ApolloProvider } from "@apollo/client";
+import { registerRootComponent } from "expo";
+import { NavigationContainer } from "@react-navigation/native";
 import {
   dependencies,
   DependencyContext,
 } from "./src/common/context/DependencyContext";
-import OnboardingNavigator from "./src/navigation/navigator/OnboardingNavigator";
 
 export const App: React.FC<{}> = () => {
   return (
-    <DependencyContext.Provider value={dependencies}>
-      <NavigationContainer>
-        <OnboardingNavigator />
-      </NavigationContainer>
-    </DependencyContext.Provider>
+    <ApolloProvider client={Client}>
+      <DependencyContext.Provider value={dependencies}>
+        <NavigationContainer>
+          <OnboardingNavigator />
+        </NavigationContainer>
+      </DependencyContext.Provider>
+    </ApolloProvider>
   );
 };
+
+registerRootComponent(App);
 
 export default App;
