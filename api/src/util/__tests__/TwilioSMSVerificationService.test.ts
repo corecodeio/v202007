10f8c9083de9bb49d2c605b04cc923136280c74c
Ktoxcon <kennethtoxro@gmail.com>
2020-08-05 11:55:13 -0600

    Update: Test result description changed

--- api/src/util/__tests__/TwilioSMSVerificationService.test.ts
+++ api/src/util/__tests__/TwilioSMSVerificationService.test.ts
@@ -3,7 +3,7 @@ import smsVerification from "../twilio/service/SMSVerification";
 describe("SMSVerification Service", () => {
   const { send, verify } = smsVerification;
 
-  it("Should return true if response has status code 'approved'", async () => {
+  it("Should return true if response has status code is 'pending'", async () => {
     const to = "+50245075393";
     const response = await send(to);
     expect(response).toBe(true);
