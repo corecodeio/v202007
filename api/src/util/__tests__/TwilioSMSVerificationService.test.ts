6b4580228e17283594bb436bb6aaa3791ebc65c8
Ktoxcon <kennethtoxro@gmail.com>
2020-08-05 13:10:38 -0600

    Update: refactoring

--- api/src/util/__tests__/TwilioSMSVerificationService.test.ts
+++ api/src/util/__tests__/TwilioSMSVerificationService.test.ts
@@ -4,7 +4,7 @@ describe("SMSVerification Service", () => {
   const { send, verify } = smsVerification;
 
   it("Should return true if response has status code is 'pending'", async () => {
-    const to = "+50245075393";
+    const to = process.env.TWILIO_SMSVERIFICATION_TEST_NUM;
     const response = await send(to);
     expect(response).toBe(true);
   });
