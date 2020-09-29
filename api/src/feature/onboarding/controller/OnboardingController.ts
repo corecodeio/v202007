3080c78774b6687f4bfc038194a4956f22c86121
MConcoba <mconcoba20@gmail.com>
2020-09-29 17:15:18 -0600

    Transform library migration

--- api/src/feature/onboarding/controller/OnboardingController.ts
+++ api/src/feature/onboarding/controller/OnboardingController.ts
@@ -1,59 +1,56 @@
-import { prisma } from "@corecodeio/database";
-import config from "../../../util/config";
-import { ISession } from "../../../util/session/interface/ISession";
-import formatPhoneNumber from "../../../util/transform/formatPhoneNumber";
-import { ISMSVerification } from "../../../util/twilio/interface/ISMSVerification";
-import { IOnboardingController } from "../interface/IOnboardingController";
+import { prisma } from '@corecodeio/database';
+import { formatPhoneNumber } from '@corecodeio/libraries/transform';
+import config from '../../../util/config';
+import { ISession } from '../../../util/session/interface/ISession';
+import { ISMSVerification } from '../../../util/twilio/interface/ISMSVerification';
+import { IOnboardingController } from '../interface/IOnboardingController';
 
-const env = config.get("env");
-const { verifiedPhoneNumbers } = config.get("flags");
+const env = config.get('env');
+const { verifiedPhoneNumbers } = config.get('flags');
 
 export class OnboardingController implements IOnboardingController {
-  private twilioSMSVerification: ISMSVerification;
-  private verifiedPhoneNumbers = [];
-  private session: ISession;
+	private twilioSMSVerification: ISMSVerification;
+	private verifiedPhoneNumbers = [];
+	private session: ISession;
 
-  constructor(twilioSMSVerification: ISMSVerification, session: ISession) {
-    this.twilioSMSVerification = twilioSMSVerification;
-    this.verifiedPhoneNumbers = env === "test" ? verifiedPhoneNumbers : [];
-    this.session = session;
-  }
+	constructor(twilioSMSVerification: ISMSVerification, session: ISession) {
+		this.twilioSMSVerification = twilioSMSVerification;
+		this.verifiedPhoneNumbers = env === 'test' ? verifiedPhoneNumbers : [];
+		this.session = session;
+	}
 
-  async verifyPhoneNumberCode({ phoneNumber, code }) {
-    const isTwilioVerified = await this.twilioSMSVerification.verify(
-      phoneNumber,
-      code
-    );
+	async verifyPhoneNumberCode({ phoneNumber, code }) {
+		const isTwilioVerified = await this.twilioSMSVerification.verify(phoneNumber, code);
 
-    if (!isTwilioVerified) {
-      throw new Error("Error al verificar");
-    }
+		if (!isTwilioVerified) {
+			throw new Error('Error al verificar');
+		}
 
-    const number = formatPhoneNumber(phoneNumber);
+		const number = formatPhoneNumber(phoneNumber);
 
-    const user = await prisma.user.create({
-      data: {
-        contact: {
-          create: {
-            phoneNumber: {
-              create: {
-                number,
-                verifiedAt: new Date(),
-              },
-            },
-          },
-        },
-      },
-    });
+		const user = await prisma.user.create({
+			data: {
+				contact: {
+					create: {
+						phoneNumber: {
+							create: {
+								number,
+								verifiedAt: new Date(),
+							},
+						},
+					},
+				},
+			},
+		});
 
-    return this.session.createOnboardingSession(user);
-  }
+		return this.session.createOnboardingSession(user);
+	}
 
-  async sendPhoneNumberVerificationCode({ phoneNumber }) {
-    if (await this.verifiedPhoneNumbers.includes(phoneNumber)) {
-      return true;
-    }
+	async sendPhoneNumberVerificationCode({ phoneNumber }) {
+		if (await this.verifiedPhoneNumbers.includes(phoneNumber)) {
+			return true;
+		}
 
-    return await this.twilioSMSVerification.send(phoneNumber);
-  }
+		return await this.twilioSMSVerification.send(phoneNumber);
+	}
 }
