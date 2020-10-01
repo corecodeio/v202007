f06990a1fa4378a6097c73cabca1024867cef568
Gus <gus@core-code.io>
2020-09-30 23:00:15 -0600

    Applies prettier

--- api/src/middleware/phoneNumberExist/index.ts
+++ api/src/middleware/phoneNumberExist/index.ts
@@ -1,23 +1,26 @@
-import prisma from '@corecodeio/database';
-import { VerifyPhoneNumberCodeInput } from '@corecodeio/libraries/api';
-import { formatPhoneNumber } from '@corecodeio/libraries/transform';
-import { IMiddlewareAsync } from '../interface/IMiddlewareAsync';
+import prisma from "@corecodeio/database";
+import { VerifyPhoneNumberCodeInput } from "@corecodeio/libraries/api";
+import { formatPhoneNumber } from "@corecodeio/libraries/transform";
+import { IMiddlewareAsync } from "../interface/IMiddlewareAsync";
 
-export class PhoneNumberExistMiddleware implements IMiddlewareAsync<VerifyPhoneNumberCodeInput> {
-	async isValid(input) {
-		const number = formatPhoneNumber(input.phoneNumber);
+export class PhoneNumberExistMiddleware
+  implements IMiddlewareAsync<VerifyPhoneNumberCodeInput> {
+  async isValid(input) {
+    const number = formatPhoneNumber(input.phoneNumber);
 
-		const phoneNumbers = await prisma.phoneNumber.findMany({
-			where: {
-				number,
-			},
-			select: {
-				verifiedAt: true,
-			},
-		});
+    const phoneNumbers = await prisma.phoneNumber.findMany({
+      where: {
+        number,
+      },
+      select: {
+        verifiedAt: true,
+      },
+    });
 
-		const isPhoneNumberVerified = phoneNumbers.filter((phoneNumber) => phoneNumber.verifiedAt !== null);
+    const isPhoneNumberVerified = phoneNumbers.filter(
+      (phoneNumber) => phoneNumber.verifiedAt !== null
+    );
 
-		return isPhoneNumberVerified.length === 0;
-	}
+    return isPhoneNumberVerified.length === 0;
+  }
 }
