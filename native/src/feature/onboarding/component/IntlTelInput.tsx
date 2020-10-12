7cad2782e2ac3ba9c2613b968c4367437922351e
Gus <gus@core-code.io>
2020-10-12 16:25:25 -0600

    Applies prettier

--- native/src/feature/onboarding/component/IntlTelInput.tsx
+++ native/src/feature/onboarding/component/IntlTelInput.tsx
@@ -1,60 +1,62 @@
-import React, { useRef, useState } from 'react';
-import { StyleSheet } from 'react-native';
-import PhoneInput from 'react-native-phone-number-input';
+import React, { useRef, useState } from "react";
+import { StyleSheet } from "react-native";
+import PhoneInput from "react-native-phone-number-input";
 
 type Props = { onSetPhoneNumber: (text: string) => void };
 
 const IntlTelInput: React.FC<Props> = ({ onSetPhoneNumber }) => {
-	const [value, setValue] = useState('');
-	const [formattedValue, setFormattedValue] = useState('');
-	const [valid, setValid] = useState(false);
-	const phoneInput = useRef<PhoneInput>(null);
+  const [value, setValue] = useState("");
+  const [formattedValue, setFormattedValue] = useState("");
+  const [valid, setValid] = useState(false);
+  const phoneInput = useRef<PhoneInput>(null);
 
-	const onChangePhoneNumber = (number: string) => {
-		setValue('+502' + number);
-		{
-			const checkValid = phoneInput.current?.isValidNumber(number);
-			setValid(checkValid ? checkValid : false);
+  const onChangePhoneNumber = (number: string) => {
+    setValue("+502" + number);
+    {
+      const checkValid = phoneInput.current?.isValidNumber(number);
+      setValid(checkValid ? checkValid : false);
 
-			phoneInput.current?.isValidNumber(number) ? onSetPhoneNumber('+502' + number) : onSetPhoneNumber('');
-		}
-	};
+      phoneInput.current?.isValidNumber(number)
+        ? onSetPhoneNumber("+502" + number)
+        : onSetPhoneNumber("");
+    }
+  };
 
-	return (
-		<PhoneInput
-			containerStyle={styles.container}
-			codeTextStyle={styles.textInput}
-			textInputStyle={styles.textInput}
-			textContainerStyle={styles.textContainer}
-			placeholder="Número de Teléfono"
-			ref={phoneInput}
-			defaultValue={value}
-			defaultCode="GT"
-			onChangeText={onChangePhoneNumber}
-			onChangeFormattedText={(text) => {
-				setFormattedValue(text);
-			}}
-			withDarkTheme
-			autoFocus
-		/>
-	);
+  return (
+    <PhoneInput
+      containerStyle={styles.container}
+      codeTextStyle={styles.textInput}
+      textInputStyle={styles.textInput}
+      textContainerStyle={styles.textContainer}
+      placeholder="Número de Teléfono"
+      ref={phoneInput}
+      defaultValue={value}
+      defaultCode="GT"
+      onChangeText={onChangePhoneNumber}
+      onChangeFormattedText={(text) => {
+        setFormattedValue(text);
+      }}
+      withDarkTheme
+      autoFocus
+    />
+  );
 };
 
 const styles = StyleSheet.create({
-	textInput: {
-		fontSize: 12,
-	},
-	container: {
-		marginLeft: 19,
-		justifyContent: 'center',
-		alignItems: 'center',
-	},
-	textContainer: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#fff',
-	},
+  textInput: {
+    fontSize: 12,
+  },
+  container: {
+    marginLeft: 19,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
 });
 
 export default IntlTelInput;
--- native/src/feature/onboarding/screen/SendPhoneNumberVerificationCode.tsx
+++ native/src/feature/onboarding/screen/SendPhoneNumberVerificationCode.tsx
@@ -1,74 +1,95 @@
-import { QuerySendPhoneNumberVerificationCodeArgs } from '@corecodeio/libraries/api';
-import { StackNavigationProp } from '@react-navigation/stack';
-import React from 'react';
-import { Text } from 'react-native';
-import { FooterText, FooterView, PrimaryText, SecondaryText, TertiaryText, View } from '../../../common/component';
-import { PrimaryButton } from '../../../common/component/Button';
-import { DependencyContext } from '../../../common/context/DependencyContext';
-import { OnboardingStackScreenName } from '../../../navigation/model/OnboardingStackScreenName';
-import { OnboardingStackParamList } from '../../../navigation/types/OnboardingStackParamList';
-import IntlTelInput from '../component/IntlTelInput';
-import { OnboardingInjectionKey } from '../InjectionKey';
+import { QuerySendPhoneNumberVerificationCodeArgs } from "@corecodeio/libraries/api";
+import { StackNavigationProp } from "@react-navigation/stack";
+import React from "react";
+import { Text } from "react-native";
+import {
+  FooterText,
+  FooterView,
+  PrimaryText,
+  SecondaryText,
+  TertiaryText,
+  View,
+} from "../../../common/component";
+import { PrimaryButton } from "../../../common/component/Button";
+import { DependencyContext } from "../../../common/context/DependencyContext";
+import { OnboardingStackScreenName } from "../../../navigation/model/OnboardingStackScreenName";
+import { OnboardingStackParamList } from "../../../navigation/types/OnboardingStackParamList";
+import IntlTelInput from "../component/IntlTelInput";
+import { OnboardingInjectionKey } from "../InjectionKey";
 
 type Props = {
-	navigation: StackNavigationProp<
-		OnboardingStackParamList,
-		OnboardingStackScreenName.SendPhoneNumberVerificationCode
-	>;
+  navigation: StackNavigationProp<
+    OnboardingStackParamList,
+    OnboardingStackScreenName.SendPhoneNumberVerificationCode
+  >;
 };
 
-export const SendPhoneNumberVerificationCode: React.FC<Props> = ({ navigation }) => {
-	const dependencies = React.useContext(DependencyContext);
-	const onboarding = dependencies.provide(OnboardingInjectionKey);
+export const SendPhoneNumberVerificationCode: React.FC<Props> = ({
+  navigation,
+}) => {
+  const dependencies = React.useContext(DependencyContext);
+  const onboarding = dependencies.provide(OnboardingInjectionKey);
 
-	const [args, setInput] = React.useState<QuerySendPhoneNumberVerificationCodeArgs>({
-		input: {
-			phoneNumber: '',
-		},
-	});
+  const [args, setInput] = React.useState<
+    QuerySendPhoneNumberVerificationCodeArgs
+  >({
+    input: {
+      phoneNumber: "",
+    },
+  });
 
-	const {
-		executeSendPhoneNumberVerificationCode,
-		result,
-		queryResult: { error: sendPhoneNumberVerificationCodeError },
-	} = onboarding.useSendPhoneNumberVerificationCode();
+  const {
+    executeSendPhoneNumberVerificationCode,
+    result,
+    queryResult: { error: sendPhoneNumberVerificationCodeError },
+  } = onboarding.useSendPhoneNumberVerificationCode();
 
-	React.useEffect(() => {
-		if (!Boolean(result)) {
-			return;
-		}
+  React.useEffect(() => {
+    if (!Boolean(result)) {
+      return;
+    }
 
-		navigation.navigate(OnboardingStackScreenName.VerifyPhoneNumberCode, {
-			phoneNumber: args.input.phoneNumber,
-		});
-	}, [result]);
+    navigation.navigate(OnboardingStackScreenName.VerifyPhoneNumberCode, {
+      phoneNumber: args.input.phoneNumber,
+    });
+  }, [result]);
 
-	const onSetPhoneNumber = (phoneNumber: string) => {
-		setInput({ input: { phoneNumber } });
-	};
+  const onSetPhoneNumber = (phoneNumber: string) => {
+    setInput({ input: { phoneNumber } });
+  };
 
-	const onSendPhoneNumberVerificationCode = async () => {
-		try {
-			await executeSendPhoneNumberVerificationCode(args);
-		} catch (error) {}
-	};
+  const onSendPhoneNumberVerificationCode = async () => {
+    try {
+      await executeSendPhoneNumberVerificationCode(args);
+    } catch (error) {}
+  };
 
-	return (
-		<View container flex={1} justifyContent="center">
-			<PrimaryText>MariaApp</PrimaryText>
-			<SecondaryText paddingTop={30}>Selecciona tu código de país e ingresa tu numero de teléfono</SecondaryText>
-			{/* <TextInput onChange={(e) => onSetPhoneNumber(e.target.valueOf())} /> */}
-			<IntlTelInput onSetPhoneNumber={onSetPhoneNumber} />
-			{sendPhoneNumberVerificationCodeError && <Text>Error al enviar el SMS. Intenta de nuevo.</Text>}
-			<PrimaryButton mb={4} onPress={onSendPhoneNumberVerificationCode} disabled={!args.input.phoneNumber}>
-				Continuar
-			</PrimaryButton>
-			<TertiaryText>Al crear tu cuenta aceptas los Términos y Condiciones</TertiaryText>
-			<FooterView container>
-				<FooterText>¿Ya tienes cuenta?</FooterText>
-			</FooterView>
-		</View>
-	);
+  return (
+    <View container flex={1} justifyContent="center">
+      <PrimaryText>MariaApp</PrimaryText>
+      <SecondaryText paddingTop={30}>
+        Selecciona tu código de país e ingresa tu numero de teléfono
+      </SecondaryText>
+      {/* <TextInput onChange={(e) => onSetPhoneNumber(e.target.valueOf())} /> */}
+      <IntlTelInput onSetPhoneNumber={onSetPhoneNumber} />
+      {sendPhoneNumberVerificationCodeError && (
+        <Text>Error al enviar el SMS. Intenta de nuevo.</Text>
+      )}
+      <PrimaryButton
+        mb={4}
+        onPress={onSendPhoneNumberVerificationCode}
+        disabled={!args.input.phoneNumber}
+      >
+        Continuar
+      </PrimaryButton>
+      <TertiaryText>
+        Al crear tu cuenta aceptas los Términos y Condiciones
+      </TertiaryText>
+      <FooterView container>
+        <FooterText>¿Ya tienes cuenta?</FooterText>
+      </FooterView>
+    </View>
+  );
 };
 
 export default SendPhoneNumberVerificationCode;
