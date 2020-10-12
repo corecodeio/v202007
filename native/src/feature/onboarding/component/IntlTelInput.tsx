d5e32a51170f86ccfc7a38cd853c7c3709af08aa
Gus <gus@core-code.io>
2020-10-12 16:54:49 -0600

    uses formatted text

--- native/src/feature/onboarding/component/IntlTelInput.tsx
+++ native/src/feature/onboarding/component/IntlTelInput.tsx
@@ -1,41 +1,30 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 import { StyleSheet } from "react-native";
 import PhoneInput from "react-native-phone-number-input";
 
 type Props = { onSetPhoneNumber: (text: string) => void };
 
 const IntlTelInput: React.FC<Props> = ({ onSetPhoneNumber }) => {
-  const [value, setValue] = useState("");
-  const [formattedValue, setFormattedValue] = useState("");
-  const [valid, setValid] = useState(false);
-  const phoneInput = useRef<PhoneInput>(null);
+  const [value, setValue] = React.useState("");
+  const [, setFormattedValue] = React.useState("");
+  const phoneInput = React.useRef<PhoneInput>(null);
 
-  const onChangePhoneNumber = (number: string) => {
-    setValue("+502" + number);
-    {
-      const checkValid = phoneInput.current?.isValidNumber(number);
-      setValid(checkValid ? checkValid : false);
-
-      phoneInput.current?.isValidNumber(number)
-        ? onSetPhoneNumber("+502" + number)
-        : onSetPhoneNumber("");
-    }
+  const onChangeFormattedText = (text: string) => {
+    setFormattedValue(text);
+    onSetPhoneNumber(text);
   };
 
   return (
     <PhoneInput
+      ref={phoneInput}
       containerStyle={styles.container}
       codeTextStyle={styles.textInput}
       textInputStyle={styles.textInput}
       textContainerStyle={styles.textContainer}
       placeholder="Número de Teléfono"
-      ref={phoneInput}
       defaultValue={value}
       defaultCode="GT"
-      onChangeText={onChangePhoneNumber}
-      onChangeFormattedText={(text) => {
-        setFormattedValue(text);
-      }}
+      onChangeFormattedText={onChangeFormattedText}
       withDarkTheme
       autoFocus
     />
